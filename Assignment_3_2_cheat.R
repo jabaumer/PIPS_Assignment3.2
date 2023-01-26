23968726ca7e2cd4dc155a4092f642a823ef1600
Janina Baumer <123576681+jabaumer@users.noreply.github.com>
2023-01-26 11:50:46 +0100

    Add files via upload

---
+++ Assignment_3_2_cheat.R
@@ -0,0 +1,15 @@
+#cheat function:
+#this function can return the solutions for exercises 1, 16 and 17
+
+cheat <- function(exercise){
+  if(exercise == 1){
+    print("exam_grades <- rnorm(65,7.5,1)")
+    print("hist(exam_grades)")
+  } else if (exercise == 16){
+    print("matrix(1:9, nrow = 3, byrow = TRUE)*c(1:3)")
+  } else if (exercise == 17){
+    print("#control + shift + A")
+  } else {
+    print("Sorry, you can't get the solution for this exercise!")
+  }
+}
---
+++ Assignment_3_2_remind_me.R
@@ -0,0 +1,6 @@
+#remind_me function:
+
+remind_me <- function(){
+  groceries <- c("rice", "chocolate", "cheese", "tomatoes", "strawberries", "bananas", "potatoes")
+  return(as.list(groceries))
+}
